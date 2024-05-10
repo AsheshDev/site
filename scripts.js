5f75ce2ab5dac7ee111dcb959a1a44f6c5c7abe2
AsheshDev <168283730+AsheshDev@users.noreply.github.com>
2024-05-10 17:37:39 +0800

    Create scripts.js

---
+++ scripts.js
@@ -0,0 +1,8 @@
+// JavaScript for interactive elements
+window.addEventListener('DOMContentLoaded', (event) => {
+  var music = document.getElementById('background-music');
+  music.volume = 0.2; // Set the volume to 20% initially
+});
+
+// Roadmap scrolling functionality
+// Add your JavaScript for the scrolling roadmap here
